ffdff77e9924e999fef84132e606bd79c4035a10
Christian Engel <engel.christian1994@gmail.com>
2021-07-25 20:12:37 +0200

    Add levenberg marquardt algo to calcXt

--- Computer Vision II/matlab/app_get_xt.m
+++ Computer Vision II/matlab/app_get_xt.m
@@ -3,15 +3,17 @@ clear;
 syms x;
 
 % TODO: write down the equations
+Max = @(aa, bb) piecewise(aa > bb, aa, bb);
 r1 = sign(x) * abs(x)^(1/3);
 r2 = x^2;
-r3 = 2 * x - 1; % wtf
+r3 = Max(x, 2 * x - 1); % wtf
 
 % Tip: try the easy diffs first
 r = r2; % TODO adjust r here in the exam
 x0 = 2; % TODO adjust
-x1 = calcXt(r, x0);
-x2 = calcXt(r, x1);
+mode = 'lm'; % TODO choose either gn or lm
+x1 = calcXt(r, x0, mode);
+x2 = calcXt(r, x1, mode);
 
 fprintf('x1 = %s \n', rats(x1));
 fprintf('x2 = %s \n', rats(x2));
--- Computer Vision II/matlab/calcXt.m
+++ Computer Vision II/matlab/calcXt.m
@@ -1,8 +1,23 @@
-function [xt_new] = calcXt(r, xt)
+function [xt_new] = calcXt(r, xt, mode)
     % r is a function
     % xt is the value of x at iteration t
+    % mode defines the algorithm to use
+    % Gauss-Newton or Levenberg-Marquardt
     % returns the value of x after iteration t+1
-    r_prime = diff(r);
-    r_triangle = -subs(r, xt) * inv(subs(r_prime, xt));
-    xt_new = xt + r_triangle;
+    if strcmp(mode, 'gn') % Gauss-Newton
+        r_prime = diff(r);
+        r_triangle = -subs(r, xt) * inv(subs(r_prime, xt));
+        xt_new = xt + r_triangle;
+    elseif strcmp(mode, 'lm')
+        % Levenberg-Marquardt
+        lambda = 0; % TODO adjust if given
+        r_prime = diff(r);
+        r = subs(r, xt);
+        r_prime = subs(r_prime, xt);
+        r_triangle = -inv(r_prime * r_prime + lambda * r_prime * r_prime) * r_prime * r;
+        xt_new = xt + r_triangle;
+    else
+        fprintf('choose a valid algorithm');
+    end
+
 end
