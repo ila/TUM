2afbcf526068b5f3f2b63242e9345d61e915eecc
Christian Engel <engel.christian1994@gmail.com>
2021-07-25 22:01:25 +0200

    Add gradient descent and newton method

--- Computer Vision II/matlab/app_get_xt.m
+++ Computer Vision II/matlab/app_get_xt.m
@@ -2,18 +2,26 @@ clear;
 % Exam P10 e)
 syms x;
 
-% TODO: write down the equations
+% internal max and min functionc can't be differentiated
 Max = @(aa, bb) piecewise(aa > bb, aa, bb);
-r1 = sign(x) * abs(x)^(1/3);
-r2 = x^2;
-r3 = Max(x, 2 * x - 1); % wtf
+Min = @(aa, bb) piecewise(aa < bb, aa, bb);
 
-% Tip: try the easy diffs first
-r = r2; % TODO adjust r here in the exam
-x0 = 2; % TODO adjust
-mode = 'lm'; % TODO choose either gn or lm
-x1 = calcXt(r, x0, mode);
-x2 = calcXt(r, x1, mode);
+% TODO: write down the residuals
+r1 = sign(x) * norm(x)^(1/3);
+r2 = x^2;
+r3 = Max(x, 2 * x - 1);
+
+% TODO if using gn or nt define f
+f = (x + 1)^2; % replace with the given function
+
+r = r3; % TODO adjust r
+x0 = 1; % TODO adjust for given value
+mode = 'lm'; % choose either gn or lm
+
+% you might have to modify calcXt for given lambda or step values
+% if you use lm or gd
+x1 = calcXt(f, r, x0, mode);
+x2 = calcXt(f, r, x1, mode);
 
 fprintf('x1 = %s \n', rats(x1));
 fprintf('x2 = %s \n', rats(x2));
--- Computer Vision II/matlab/calcXt.m
+++ Computer Vision II/matlab/calcXt.m
@@ -1,5 +1,7 @@
-function [xt_new] = calcXt(r, xt, mode)
-    % r is a function
+function [xt_new] = calcXt(f, r, xt, mode)
+    % f is the function needed for gradient descent and newton
+    %  => you can enter whatever value if you don't use gd or newt
+    % r is the residual function
     % xt is the value of x at iteration t
     % mode defines the algorithm to use
     % Gauss-Newton or Levenberg-Marquardt
@@ -16,6 +18,21 @@ function [xt_new] = calcXt(r, xt, mode)
         r_prime = subs(r_prime, xt);
         r_triangle = -inv(r_prime * r_prime + lambda * r_prime * r_prime) * r_prime * r;
         xt_new = xt + r_triangle;
+    elseif strcmp(mode, 'gd')
+        % Gradient Descent
+        step = 3; % TODO adjust if given
+        f_prime = diff(f);
+        f_prime = subs(f_prime, xt);
+        f_triangle =- step * f_prime;
+        xt_new = xt + f_triangle;
+    elseif strcmp(mode, 'newt')
+        % Newton
+        f_prime = diff(f);
+        f_prime = subs(f_prime, xt);
+        h = diff(diff(f));
+        h = subs(h, xt);
+        f_triangle =- f_prime * inv(h);
+        xt_new = xt + f_triangle;
     else
         fprintf('choose a valid algorithm');
     end
