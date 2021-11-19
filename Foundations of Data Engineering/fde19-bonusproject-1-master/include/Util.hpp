75559dde6f53c3bdb372eb4d571e8fabd6256233
ila <ilaria.battiston@gmail.com>
2021-11-19 18:26:07 +0100

    removed some projects

--- Foundations of Data Engineering/fde19-bonusproject-1-master/include/Util.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-//---------------------------------------------------------------------------
-inline std::string getDir(const std::string &file)
-/// Returns path to file in 'file'
-{
-   size_t found = file.find_last_of("/\\");
-   return (file.substr(0, found));
-}
-//---------------------------------------------------------------------------
--- Foundations of Data Engineering/fde19-bonusproject-3-master/include/DistCalculator.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-class DistCalculator{
-public:
-  using Node = uint64_t;
-  DistCalculator(std::string edgeListFile);
-  int64_t dist(Node a, Node b);
-};
